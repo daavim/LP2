757cc58539985ba794c638fe3f981a7f3fbce70f
admin <admin@admin.com>
2025-01-23 01:03:35 -0300

    Estruturação

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
